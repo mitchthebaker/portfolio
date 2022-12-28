a22b75df86930857022da73720ddc67752345104
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-12-27 21:26:37 -0800

    changes

--- src/components/content/WorkContent.jsx
+++ src/components/content/WorkContent.jsx
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import ExpContainer from './ExpContainer';
